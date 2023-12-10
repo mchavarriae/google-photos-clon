0b79b538e9a976287223f77106ad79b405ecf031
Marlon Chavarria <marlonc@tevixmd.com>
2023-12-10 11:59:37 -0600

    Docker file and env variables for app-js

--- app-js/index.js
+++ app-js/index.js
@@ -5,21 +5,24 @@ const Firestore = require('@google-cloud/firestore');
 
 
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 8080;
+const PROJECT_ID = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT
+const BUCKET = process.env.BUCKET;
+
 app.set('view engine', 'ejs');
 
 
 const db = new Firestore({
-  projectId: 'pragmatic-cache-403800',
+  projectId: PROJECT_ID,
   timestampsInSnapshots: true
 });
 // Configure Google Cloud Storage
 const storage = new Storage({
-  projectId: 'pragmatic-cache-403800'
+  projectId: PROJECT_ID 
 });
 
-const bucketName = 'mce-ulacit-11';
-const bucket = storage.bucket(bucketName);
+
+const bucket = storage.bucket(BUCKET);
 
 // Configure Multer for file uploads
 const multer = Multer({
@@ -66,7 +69,7 @@ app.post('/upload', multer.single('file'), async (req, res) => {
     const blobStream = blob.createWriteStream();
 
     blobStream.on('finish', () => {
-      const publicUrl = `https://storage.googleapis.com/${bucketName}/${blob.name}`;
+      const publicUrl = `https://storage.googleapis.com/${BUCKET}/${blob.name}`;
       // res.status(200).send(`Archivo subido, satisfactoriamente`);
       res.redirect('/');
     });
